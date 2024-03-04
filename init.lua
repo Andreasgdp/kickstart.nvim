72ac9c30e18f54c57cd438e8fe9cb6f162b4b0ff
Andreas Petersen <andreasgdp@gmail.com>
2024-03-04 12:56:12 +0100

    Save all files with ctrl + s and not just current buffer

--- init.lua
+++ init.lua
@@ -828,6 +828,6 @@ require('lazy').setup {
 vim.keymap.set('n', '<leader>gg', vim.cmd.LazyGit, { desc = 'Opens LazyGit within NeoVim' })
 -- TODO: make this a toggle instead of just opening
 vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle last<cr><esc>', { desc = 'Toggles Neo-Tree file tree' })
-vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>wa<cr><esc>', { desc = 'Save file' })
 -- TODO: add quitting guard from lazyvim with y, n, cancel selections for saving before quitting
 vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
